67d8fdc8fbc5ae177925f1bd2bd45d1110802fa5
Chiyu710 <Chiyu.wang01@gmail.com>
2019-06-10 20:26:50 +0800

    大改

--- PIEA/PIEA/Grade.h
+++ PIEA/PIEA/Grade.h
@@ -1,6 +1,13 @@
+#include<iomanip>
 using namespace std;
 class cgrade 
 {
+	int math;
+	int lang;
+	int policy;
+	int pro;
+	int sum;
+	string major;
 public:
 	void setcgrade(int g1=0,int g2=0,int g3=0,int g4=0,int s1=0) 
 	{
@@ -9,12 +16,24 @@ public:
 		policy = g3;
 		pro = g4;
 		sum = s1;
-		cout << "修改成功！";
+		cout << "输入成功！"<<endl;
 	}
-	int math;
-	int lang;
-	int policy;
-	int pro;
-	int sum;
+	string get_major() { return major; }
+	void gradelist();
+	void set_major(string s) { major = s; }
+	int   get_math() { return math; }
+	int   get_lang() { return lang; }
+	int   get_policy() { return policy; }
+	int   get_pro() { return pro; }
+	int   get_sum() { return sum; }
 };
 
+void cgrade::gradelist() 
+{
+	cout << left << setw(10) <<"数学:"<< math << endl;
+	cout << left << setw(10) <<"语言:" <<lang<<endl;
+	cout << left << setw(10) <<"政治:"<< policy<<endl;
+	cout << left << setw(10) <<"专业课:"<< pro<<endl;
+	cout << left << setw(10) << "总分:"<<sum<<endl;
+	cout << endl;
+}
--- PIEA/PIEA/Students.h
+++ PIEA/PIEA/Students.h
@@ -1,32 +1,60 @@
 #include <cstring>
 #include<fstream>
+#include <iomanip>
 using namespace std;
 class students
 {
-public:
-	char name[10];   
-	char id[20];     
-	char major[10];  
-	int math;        
-	int lang;       
-	int policy;      
-	int pro;        
-	int Sum;       
-	char is_year[3];   
+	char name[10];
+	char id[20];
+	char major[10];
+	int math;
+	int lang;
+	int policy;
+	int pro;
+	int sum;
+	char is_year[3];
 	students *next;
-	void get_sum() { Sum = math + lang + pro + policy; }
+public:
+	char* get_nam()    { return name; }
+	char* get_id()     { return id; }
+	char* get_major()  { return major; }
+	int   get_math()   { return math; }
+	int   get_lang()   { return lang; }
+	int   get_policy() { return policy; }
+	int   get_pro()    { return pro; }
+	int   get_sum()    { return sum; }
+	students* get_next() { return next; }
+	void set_sum() { sum = math + lang + pro + policy; }
+	void set_name(char str[10]) { strcpy_s(name, str); }
+	void set_id(char str[20])   { strcpy_s(id, str); }
+	void set_major(char str[10]) { strcpy_s(major, str); }
+	void set_math(int grade)     { math = grade; }
+	void set_lang(int grade)   { lang = grade; }
+	void set_policy(int grade)  { policy = grade; }
+	void set_pro(int grade)     { pro = grade; }
+	void set_is(char str[3]) { strcpy_s(is_year, str); }
+	void set_next(students * pstu) { next=pstu; }
 	void deletestu(students * s, students *f);
 	void list();
 	void edit(char tname[], char ttid[], char  tmajor[], char tis[], int g1, int g2, int g3, int g4);
 	void input(students* stu);
-	void pinput(students* stu);
-	//students operator=()
+	void pinput(students* stu, int num);
+	void scpy(students *obj);
+	void plist();
+	void ErrorCheck();
 };
 
 void students::list()
 {
-	cout << name << '\t' << id << '\t' << major << '\t' << is_year <<'\t'<< math << '\t' << lang << '\t' << policy << '\t' << pro << '\t' << Sum << endl;
-
+	cout << left << setw(6) << name;
+	cout << right << setw(7) << id;
+	cout << right << setw(10) << major;
+	cout << right << setw(10) << is_year;
+	cout << right << setw(10) << math;
+	cout << right << setw(10) << lang; 
+	cout << right << setw(10) << policy;
+	cout << right << setw(10) << pro;
+	cout << endl;
 }
 
 void students::edit(char tname[], char ttid[],char  tmajor[], char tis[], int g1, int g2, int g3, int g4)
@@ -40,7 +68,7 @@ void students::edit(char tname[], char ttid[],char  tmajor[], char tis[], int g1
 	policy = g3;
 	pro = g4;
 	this->get_sum();
-	cout << "修改成功" << endl;
+	cout << "操作成功" << endl;
 }
 
 void students::input(students* stu)
@@ -48,14 +76,14 @@ void students::input(students* stu)
 	ofstream fout("studentsinfo.txt", ios::out);
 	while (stu != NULL)
 	{
-		fout << stu->name << '\t';
-		fout << stu->id << '\t';
-		fout << stu->major << '\t';
-		fout << stu->is_year << '\t';
-		fout << stu->math << '\t';
-		fout << stu->lang << '\t';
-		fout << stu->policy << '\t';
-		fout << stu->pro << '\t';
+		fout << left << setw(6) << stu->name ;
+		fout << right << setw(7) << stu->id ;
+		fout << right << setw(10) << stu->major ;
+		fout << right << setw(10) << stu->is_year;
+		fout << right << setw(10) << stu->math ;
+		fout << right << setw(10) << stu->lang ;
+		fout << right << setw(10) << stu->policy ;
+		fout << right << setw(10) << stu->pro ;
 		fout << endl;
 		stu = stu->next;
 	}
@@ -63,6 +91,13 @@ void students::input(students* stu)
 
 void students::deletestu(students * s,students *f) 
 {
+	if (s == f) 
+	{
+		f = f->next;
+		students *ts = new students;
+		ts->scpy(f); f->scpy(s);  s->scpy(ts);
+		delete ts;
+	}
 	while (s != NULL) 
 	{
 		if (s->next==f)
@@ -76,30 +111,62 @@ void students::deletestu(students * s,students *f)
 	}
 }	
 
-void students::pinput(students* stu) 
+void students::pinput(students* stu,int num) 
 {
 	ofstream fout("PassStudentsinfo.txt", ios::out);
-	while (stu != NULL)
+	while (num>0)
 	{
-		fout << stu->name << '\t';
-		fout << stu->id << '\t';
-		fout << stu->major << '\t';
-		fout << stu->is_year << '\t';
-		fout << stu->math << '\t';
-		fout << stu->lang << '\t';
-		fout << stu->policy << '\t';
-		fout << stu->pro << '\t';
+		num--;
+		fout << left << setw(6) << stu->name;
+		fout << right << setw(7) << stu->id;
+		fout << right << setw(10) << stu->major;
+		fout << right << setw(10) << stu->is_year;
+		fout << right << setw(10) << stu->math;
+		fout << right << setw(10) << stu->lang;
+		fout << right << setw(10) << stu->policy;
+		fout << right << setw(10) << stu->pro;
+		fout << right << setw(10) << stu->sum;
 		fout << endl;
 		stu = stu->next;
 	}
 }
 
+void students::scpy(students *obj)
+{
+	strcpy_s(name, obj->name);
+	strcpy_s(id, obj->id);
+	strcpy_s(major, obj->major);
+	strcpy_s(is_year, obj->is_year);
+	math = obj->math;
+	lang = obj->lang;
+	policy = obj->policy;
+	pro = obj->pro;
+	sum = obj->sum;
+}
 
-/*void set_name(char str[10]) { strcpy_s(name, str); }
-	void set_id(char str[20])   { strcpy_s(id, str); }
-	void set_major(char str[10]) { strcpy_s(major, str); }
-	void set_math(int grade)     { math = grade; }
-	void set_lang(int grade)   { lang = grade; }
-	void set_policy(int grade)  { policy = grade; }
-	void set_pro(int grade)     { pro = grade; }
-	void set_is(char str[3]) { strcpy_s(is_year, str); }*/
+void students::plist()
+{
+	cout << left << setw(6) << name;
+	cout << right << setw(7) << id;
+	cout << right << setw(10) << major;
+	cout << right << setw(10) << is_year;
+	cout << right << setw(10) << math;
+	cout << right << setw(10) << lang;
+	cout << right << setw(10) << policy;
+	cout << right << setw(10) << pro;
+	cout << right << setw(10) << sum;
+	cout << endl;
+}
+
+void students::ErrorCheck() 
+{
+	if (math > 100 || lang > 150 || policy > 100 || pro > 150)
+	{
+		list();
+		if (math > 100)   cout << "数学成绩有误！   ";
+		if (lang > 150)   cout << "语言成绩有误！   ";
+		if (policy > 100) cout << "政治成绩有误！   ";
+		if (pro > 150)    cout << "专业成绩有误！   ";
+		cout << endl<<endl;
+	}
+}
