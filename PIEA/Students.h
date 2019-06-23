6cc462070e92e9764d63a8346cf4aa249199c6fa
Chiyu710 <Chiyu.wang01@gmail.com>
2019-06-23 12:10:05 +0800

    排序功能修改

--- PIEA/Students.h
+++ PIEA/Students.h
@@ -4,30 +4,57 @@
 using namespace std;
 class students
 {
-public:
-	char name[10];   
-	char id[20];     
-	char major[10];  
-	int math;        
-	int lang;       
-	int policy;      
-	int pro;        
-	int Sum;       
-	char is_year[3];   
+	char name[10];
+	char id[20];
+	char major[10];
+	int math;
+	int lang;
+	int policy;
+	int pro;
+	int sum;
+	char is_year[3];
 	students *next;
-	void get_sum() { Sum = math + lang + pro + policy; }
+public:
+	char* get_nam()    { return name; }
+	char* get_id()     { return id; }
+	char* get_major()  { return major; }
+	int   get_math()   { return math; }
+	int   get_lang()   { return lang; }
+	int   get_policy() { return policy; }
+	int   get_pro()    { return pro; }
+	int   get_sum()    { return sum; }
+	students* get_next() { return next; }
+	void set_sum() { sum = math + lang + pro + policy; }
+	void set_name(char str[10]) { strcpy_s(name, str); }
+	void set_id(char str[20])   { strcpy_s(id, str); }
+	void set_major(char str[10]) { strcpy_s(major, str); }
+	void set_math(int grade)     { math = grade; }
+	void set_lang(int grade)   { lang = grade; }
+	void set_policy(int grade)  { policy = grade; }
+	void set_pro(int grade)     { pro = grade; }
+	void set_is(char str[3]) { strcpy_s(is_year, str); }
+	void set_next(students * pstu) { next=pstu; }
 	void deletestu(students * s, students *f);
 	void list();
 	void edit(char tname[], char ttid[], char  tmajor[], char tis[], int g1, int g2, int g3, int g4);
 	void input(students* stu);
 	void pinput(students* stu, int num);
 	void scpy(students *obj);
+	void plist();
+	void ErrorCheck();
 };
 
 void students::list()
 {
-	cout << name << '\t' << id << '\t' << major << '\t' << is_year <<'\t'<< math << '\t' << lang << '\t' << policy << '\t' << pro << '\t' << Sum << endl;
-
+	cout << left << setw(6) << name;
+	cout << right << setw(7) << id;
+	cout << right << setw(10) << major;
+	cout << right << setw(10) << is_year;
+	cout << right << setw(10) << math;
+	cout << right << setw(10) << lang; 
+	cout << right << setw(10) << policy;
+	cout << right << setw(10) << pro;
+	cout << endl;
 }
 
 void students::edit(char tname[], char ttid[],char  tmajor[], char tis[], int g1, int g2, int g3, int g4)
@@ -41,7 +68,7 @@ void students::edit(char tname[], char ttid[],char  tmajor[], char tis[], int g1
 	policy = g3;
 	pro = g4;
 	this->get_sum();
-	cout << "修改成功" << endl;
+	cout << "操作成功" << endl;
 }
 
 void students::input(students* stu)
@@ -49,14 +76,14 @@ void students::input(students* stu)
 	ofstream fout("studentsinfo.txt", ios::out);
 	while (stu != NULL)
 	{
-		fout << stu->name << '\t';
-		fout << stu->id << '\t';
-		fout << stu->major << '\t';
-		fout << stu->is_year << '\t';
-		fout << stu->math << '\t';
-		fout << stu->lang << '\t';
-		fout << stu->policy << '\t';
-		fout << stu->pro << '\t';
+		fout << left << setw(6) << stu->name ;
+		fout << right << setw(7) << stu->id ;
+		fout << right << setw(10) << stu->major ;
+		fout << right << setw(10) << stu->is_year;
+		fout << right << setw(10) << stu->math ;
+		fout << right << setw(10) << stu->lang ;
+		fout << right << setw(10) << stu->policy ;
+		fout << right << setw(10) << stu->pro ;
 		fout << endl;
 		stu = stu->next;
 	}
@@ -90,15 +117,15 @@ void students::pinput(students* stu,int num)
 	while (num>0)
 	{
 		num--;
-		fout << stu->name << '\t';
-		fout << stu->id << '\t';
-		fout << stu->major << '\t';
-		fout << stu->is_year << '\t';
-		fout << stu->math << '\t';
-		fout << stu->lang << '\t';
-		fout << stu->policy << '\t';
-		fout << stu->pro << '\t';
-		fout << stu->Sum << '\t';
+		fout << left << setw(6) << stu->name;
+		fout << right << setw(7) << stu->id;
+		fout << right << setw(10) << stu->major;
+		fout << right << setw(10) << stu->is_year;
+		fout << right << setw(10) << stu->math;
+		fout << right << setw(10) << stu->lang;
+		fout << right << setw(10) << stu->policy;
+		fout << right << setw(10) << stu->pro;
+		fout << right << setw(10) << stu->sum;
 		fout << endl;
 		stu = stu->next;
 	}
@@ -114,14 +141,32 @@ void students::scpy(students *obj)
 	lang = obj->lang;
 	policy = obj->policy;
 	pro = obj->pro;
-	Sum = obj->Sum;
+	sum = obj->sum;
 }
 
-/*void set_name(char str[10]) { strcpy_s(name, str); }
-	void set_id(char str[20])   { strcpy_s(id, str); }
-	void set_major(char str[10]) { strcpy_s(major, str); }
-	void set_math(int grade)     { math = grade; }
-	void set_lang(int grade)   { lang = grade; }
-	void set_policy(int grade)  { policy = grade; }
-	void set_pro(int grade)     { pro = grade; }
-	void set_is(char str[3]) { strcpy_s(is_year, str); }*/
+void students::plist()
+{
+	cout << left << setw(6) << name;
+	cout << right << setw(7) << id;
+	cout << right << setw(10) << major;
+	cout << right << setw(10) << is_year;
+	cout << right << setw(10) << math;
+	cout << right << setw(10) << lang;
+	cout << right << setw(10) << policy;
+	cout << right << setw(10) << pro;
+	cout << right << setw(10) << sum;
+	cout << endl;
+}
+
+void students::ErrorCheck() 
+{
+	if (math > 100 || lang > 150 || policy > 100 || pro > 150)
+	{
+		list();
+		if (math > 100)   cout << "数学成绩有误！   ";
+		if (lang > 150)   cout << "语言成绩有误！   ";
+		if (policy > 100) cout << "政治成绩有误！   ";
+		if (pro > 150)    cout << "专业成绩有误！   ";
+		cout << endl<<endl;
+	}
+}
--- PIEA/main.cpp
+++ PIEA/main.cpp
@@ -1,18 +1,24 @@
 #include<iostream>
 #include<fstream>
 #include<cstring>
+#include<string>
 #include<windows.h>
 #include "Grade.h"
 #include"Students.h"
 #include "Grade.cpp"
 using namespace std;
 int main()
-{
+{	
+	string major[30]; //专业分类数组
 	students* find(students *s, char str[20]);
 	bool check(students* stu, cgrade *s);
 	void plist(students*pstu, int num);
+	int MajorCheck(string s,string ma[30]);
+	void Majorsort(students* stu,string ma[30]);
+	void MajorAdd(cgrade[],string [],int );
 	char info[20];
 	int grade;
+	int tm = 0,mnum,psum;
 	char password[10];
 	char cpassword[10];
 	char tname[10], ttid[20], tmajor[10], tis[3];
@@ -20,58 +26,86 @@ int main()
 	ifstream passwd("password.txt",ios::in);
 	ifstream stuin("studentsinfo.txt", ios::in);
 	passwd >> cpassword;
+	students * ttp = new students;
 	students*news = new students;
 	students *stu = new students;
 	students *pstu = new students;
+	students *tmp = new students;
 	students *tail;
 	students *ptail;
-	students *fp = new students;
-	students* ff = new students;
+	students *fp;
+	students* ff;
+	students* slp;
 	cgrade*s = new cgrade;
-	stuin >> stu->name;
-	stuin >> stu->id;
-	stuin >> stu->major;
-	stuin >> stu->is_year;
-	stuin >> stu->math;
-	stuin >> stu->lang;
-	stuin >> stu->policy;
-	stuin >> stu->pro;
-	stu->get_sum();
-	stu->next = NULL;
-	pstu->next = NULL;
+	stuin >> tname;
+	stuin >> ttid;
+	stuin >> tmajor;
+	major[0] = tmajor;
+	stuin >> tis;
+	stuin >> g1;
+	stuin >> g2;
+	stuin >> g3;
+	stuin >> g4;
+	stu->set_name(tname);
+	stu->set_id(ttid);
+	stu->set_major(tmajor);
+	stu->set_is(tis);
+	stu->set_math(g1);
+	stu->set_lang(g2);
+	stu->set_policy(g3);
+	stu->set_pro(g4);
+	stu->set_sum();
+	stu->set_next(NULL);
+	pstu->set_next(NULL);
 	tail = stu;
 	ptail = pstu;
-	while (stuin>>info) 
+	while (stuin>>tname) 
 	{
 		students *p = new students;
 		students *pp = new students; //为过线学生造位置
-		strcpy_s(p->name,info);
-		stuin >>p->id;
-		stuin >> p->major;
-		stuin >> p->is_year;
-		stuin >> p->math;
-		stuin >> p->lang;
-		stuin >> p->policy;
-		stuin >> p->pro;
-		p->get_sum();
-		p->next = NULL;
-		pp->next = NULL; //为过线学生造位置
-		tail->next = p;
+		stuin >> ttid;
+		stuin >> tmajor;
+		MajorCheck(tmajor,major); //增加专业
+		stuin >> tis;
+		stuin >> g1;
+		stuin >> g2;
+		stuin >> g3;
+		stuin >> g4;
+		p->set_name(tname);
+		p->set_id(ttid);
+		p->set_major(tmajor);
+		p->set_is(tis);
+		p->set_math(g1);
+		p->set_lang(g2);
+		p->set_policy(g3);
+		p->set_pro(g4);
+		p->set_sum();
+		p->set_next(NULL);
+		pp->set_next(NULL); //为过线学生造位置
+		tail->set_next(p);
 		tail = p;
-		ptail->next = pp;//为过线学生造位置
+		ptail->set_next(pp);//为过线学生造位置
 		ptail = pp;//为过线学生造位置
 	}
-	//pstu = pstu->next;
+	//pstu = pstu->get_next();
 	stuin.close();
 	passwd.close();
 	students* infop = new students;
 	int i1 = 0, i2,temp;
+	cgrade grad[30];
+	int it=0;
+	while (major[it][0]!='\0') 
+	{
+		grad[it].set_major(major[it]);
+		it++;
+	}
+	//专业类初始化
 	while (i1 != -1)
 	{
 		i1 = 0, i2 = 0;
 		cout << "    ------------------------------------------------------\n";
 		cout << "    |           欢迎进入研究生录取系统                     |" << endl;
-		cout << "    |           1.登陆                                     |" << endl;
+		cout << "    |           1.登录                                     |" << endl;
 		cout << "    |           2.退出                                     |" << endl;
 		cout << "    ------------------------------------------------------\n";
 		cout << "               请选择：";
@@ -119,18 +153,26 @@ int main()
 									system("cls");
 									cout << "---------------------------------------------------------------\n";
 									infop = stu;
+									Majorsort(stu,major);
 									while (infop!=NULL) 
 									{
 										infop->list();
-										infop=infop->next;
+										infop=infop->get_next();
 									}
+									cout << endl<<endl<<endl;
+									cout << "------以下是错误信息-------" << endl;
+									infop = stu;
+									do { infop->ErrorCheck(); } while (infop = infop->get_next());
 									cout << "输入任何数字返回:";
 									cin >> i1;
 									break;
 								case 2: system("cls");
+									cout << "-----------------输入Exit返回---------------\n";
 									cout << "请输入要修改的学生的准考证号：";
 									char tid[20];
 									cin >> tid;
+									cout << endl;
+									if (strcmp(tid, "Exit") == 0) break;
 									infop = stu;
 									ff=find(stu, tid);
 									if (ff == NULL) cout << "未找到相应学生 请重试！";
@@ -138,26 +180,89 @@ int main()
 										ff->list();
 										cout << "请输入该学生修改后的信息" << endl;
 										cin >> tname >> ttid >> tmajor >> tis >> g1 >> g2 >> g3 >> g4;
+										if (MajorCheck(tmajor, major))
+										{
+											cout << "新增 " << tmajor << " 专业" << endl;
+											cout << "1.确定   2.取消" << endl;
+											cin >> temp;
+											if (temp == 2) break;
+											MajorAdd(grad, major, MajorCheck(tmajor, major));
+										}
 										ff->edit(tname, ttid, tmajor, tis, g1, g2, g3, g4);
+										ff->set_sum();
 										stu->input(stu);
 									};
 									Sleep(1000);
 									break;
+
 								case 3: 
 									system("cls");
 									cout << "-----------------输入Exit返回---------------\n";
-									cout << "请输入新增学生的信息：";
+									cout << "请输入学生名字：";
 									cin >> tname;
+									cout << endl;
 									if (strcmp(tname, "Exit") == 0) break;
-									cin>> ttid >> tmajor >> tis >> g1 >> g2 >> g3 >> g4;
-									news->edit(tname, ttid, tmajor, tis, g1, g2, g3, g4);
-									news->next = stu;
+									cout << "请输入准考证号：";
+									cin >> tid;
+									cout << endl;
+									infop = stu;
+									ff = find(stu, tid);
+									if (ff != NULL) 
+									{
+										cout << "该准考证号重复！" <<endl<<endl;
+										ff->list();
+										cout << endl;
+										cout << "1.覆盖   2.取消" << endl;
+										cin >> temp;
+										if (temp == 2) break;
+										else 
+										{
+											cout << "继续输入" << endl;
+											Sleep(500);
+											system("cls");
+											cout << "请依次输入专业、是否应届、数学成绩、语言成绩、政治成绩、专业成绩：";
+											cin >> tmajor >> tis >> g1 >> g2 >> g3 >> g4;
+											cout << "确定增加？" << endl;
+											cout << "1.确定   2.取消" << endl;
+											cin >> temp;
+											if (temp == 2) break;
+											if (MajorCheck(tmajor, major)) 
+											{
+												cout << "新增 " << tmajor << " 专业" << endl;
+												cout << "1.确定   2.取消" << endl;
+												cin >> temp;
+												if (temp == 2) break;
+												MajorAdd(grad, major, MajorCheck(tmajor, major));
+											}
+											ff->edit(tname, tid, tmajor, tis, g1, g2, g3, g4);
+											Sleep(1000);
+											break;
+										}
+									}
+									cout << "请依次输入专业、是否应届、数学成绩、语言成绩、政治成绩、专业成绩：";
+									cin>> tmajor >> tis >> g1 >> g2 >> g3 >> g4;
+									cout << "确定增加？" << endl;
+									cout << "1.确定   2.取消" << endl;
+									cin >> temp;
+									if (temp == 2) break;
+									if (MajorCheck(tmajor, major))
+									{
+										cout << "新增 " << tmajor << " 专业" << endl;
+										cout << "1.确定   2.取消" << endl;
+										cin >> temp;
+										if (temp == 2) break;
+										MajorAdd(grad, major, MajorCheck(tmajor, major));
+									}
+									news->edit(tname, tid, tmajor, tis, g1, g2, g3, g4);
+									news->set_next(stu);
 									stu = news;
 									stu->input(stu);
 									Sleep(1000);	break;
 								case 4:system("cls");
+									cout << "-----------------输入Exit返回---------------\n";
 									cout << "请输入要删除的学生的准考证号：";
 									cin >> tid;
+									if (strcmp(tid, "Exit") == 0) break;
 									infop = stu;
 									ff = find(stu, tid);
 									if (ff == NULL) cout << "未找到相应学生 请重试！"; 
@@ -186,6 +291,7 @@ int main()
 								cout << "----------------------------------\n";
 								cout << "1.输入最低成绩要求" << endl;
 								cout << "2.输出过线学生" << endl;
+								cout << "3.输出各专业最低成绩要求" << endl;
 								cout << "----------------------------------\n";
 								cout << "*输入-1返回" << endl;
 								cin >> i1;
@@ -194,42 +300,89 @@ int main()
 								case 1:
 									system("cls");
 									int lmath, lpolicy, llang, lpro, lsum;
-									cout << "数学最低分："; cin >> lmath;   cout << endl;
-									cout << "外语最低分："; cin >> llang;   cout << endl;
-									cout << "政治最低分："; cin >> lpolicy; cout << endl;
-									cout << "专业最低分："; cin >> lpro;    cout << endl;
-									cout << "总分最低分："; cin >> lsum;    cout << endl;
-									s->setcgrade(lmath, lpolicy, llang, lpro, lsum);
+									tm = 0;
+									while (grad[tm].get_major()[0]!='\0')
+									{
+										cout << "请输入<";
+										cout << grad[tm].get_major();
+										cout<<">专业成绩要求" << endl;
+										cout << "数学最低分："; cin >> lmath;   cout << endl;
+										cout << "外语最低分："; cin >> llang;   cout << endl;
+										cout << "政治最低分："; cin >> lpolicy; cout << endl;
+										cout << "专业最低分："; cin >> lpro;    cout << endl;
+										cout << "总分最低分："; cin >> lsum;    cout << endl;
+										grad[tm].setcgrade(lmath, lpolicy, llang, lpro, lsum);
+										tm++;
+									}
+									
 									Sleep(1000);
 									break;
 								case 2: 
-									int pnum;
+
 									system("cls");
-									pnum = 0;
 									ff = stu;
 									fp = pstu;
-									while (ff != NULL)
+									mnum = 0;
+									temp = 0;
+									psum = 0;
+									while (grad[mnum].get_major()[0] != '\0')
 									{
-										
-										if (check(ff, s)) 
+										while (ff && ff->get_major()== grad[mnum].get_major())
 										{
-											pnum++;
-											fp->scpy(ff);
-											fp = fp->next;
+											if (check(ff, grad + mnum)) 
+											{
+												fp->scpy(ff);
+												fp = fp->get_next();
+											}
+											temp++;
+											ff = ff->get_next();
 										}
-										ff = ff->next;
+										slp = pstu;
+										for (int ii = psum; ii > 0; ii--) 
+										{
+											slp = slp->get_next();
+										}
+										plist(slp, temp);	
+										psum += temp;
+										temp = psum;
+										ff = stu;
+										while (ff && temp>0) 
+										{
+											//ff->list();
+											ff = ff->get_next();
+											temp--;
+										}
+										if (ff == NULL)break;
+										mnum++;
 									}
 									fp = pstu;
-									plist(fp, pnum);
-									fp->pinput(fp,pnum);
-									while(pnum>0)
+									cout << "--------" << fp->get_major() << "-------" << endl;
+									temp = 1;
+									while (fp) 
 									{
-										fp->list();
-										fp = fp->next;
-										pnum--;
+										
+										if (fp->get_major() == grad[temp].get_major())
+										{
+											cout << "--------" << fp->get_major() << "-------" << endl;
+											temp++;
+										} 
+										fp->plist();
+										fp=fp->get_next();
 									}
-									
-									
+									pstu->pinput(pstu,psum);
+									cout << "输入任何数字返回:";
+									cin >> i1;
+									break;
+								case 3:
+									system("cls");
+									tm = 0;
+									while (grad[tm].get_major()[0] != '\0')
+									{
+										cout << "----------------------------------\n";
+										cout << "<"<< grad[tm].get_major()<< ">" << endl;
+										grad[tm].gradelist();
+										tm++;
+									}
 									cout << "输入任何数字返回:";
 									cin >> i1;
 									break;
@@ -238,7 +391,7 @@ int main()
 								}
 
 							}
-							i1 = 0;
+							i1 = 0; //  yuan0
 							system("cls");
 							break;
 						default: break;
@@ -264,7 +417,7 @@ int main()
 		default:
 
 			cout << "输入错误" << endl;
-			i1 = -1;
+			i1 = -11;
 
 			break;
 
@@ -282,10 +435,10 @@ students* find(students *s, char str[20])
 	find = s;
 	while (find != NULL)
 	{
-		if (strcmp(find->id, str) == 0) {
+		if (strcmp(find->get_id(), str) == 0) {
 		 return find;
 		}
-		find = find->next;
+		find = find->get_next();
 	}
 	return NULL;
 }
@@ -293,35 +446,95 @@ students* find(students *s, char str[20])
 bool check(students* stu,cgrade *s)
 {
 	bool f = 1;
-	if (stu->math < s->math) return  0;
-	if (stu->lang < s->lang) return  0;
-	if (stu->policy < s->policy) return  0;
-	if (stu->pro < s->pro) return  0;
-	if (stu->Sum < s->sum) return  0;
+	if (stu->get_math() < s->get_math()) return  0;
+	if (stu->get_lang() < s->get_lang()) return  0;
+	if (stu->get_policy() < s->get_policy()) return  0;
+	if (stu->get_pro() < s->get_pro()) return  0;
+	if (stu->get_sum() < s->get_sum()) return  0;
 	return  f;
 }
 
 void plist(students*pstu, int num)
 {
-	students *ts = new students;
-	students *s = pstu;
-	for (int i = 1; i <= num ; i++)
-	{
-		for (int j = 1; j <= num  - i; j++)
+	if (num == 1) ;
+	else {
+		students *ts = new students;
+		students *s = pstu;
+		for (int i = 1; i <= num; i++)
 		{
-
-			if (pstu->Sum < pstu->next->Sum)
+			for (int j = 1; j <= num - i; j++)
 			{
-				//cout << "fixed!!";
-				//pstu->list();
-				ts->scpy(pstu->next);
-				pstu->next->scpy(pstu);
-				pstu->scpy(ts);
+
+				if (pstu->get_sum() < pstu->get_next()->get_sum())
+				{
+					ts->scpy(pstu->get_next());
+					pstu->get_next()->scpy(pstu);
+					pstu->scpy(ts);
+				}
+				pstu = pstu->get_next();
 			}
-			pstu = pstu->next;
+			pstu = s;
 		}
 		pstu = s;
 	}
-	pstu = s;
 }
 
+int MajorCheck(string s, string ma[30])
+{
+	int n=0;
+	for (int i = 0; i < 30; i++)
+	{
+		if (s == ma[i]) break;
+		if (ma[i][0] == '\0') {n = i; break;}
+	}
+	if (n != 0) 
+	{
+		ma[n] = s;
+	}
+	return n;
+}
+
+void Majorsort(students* stu, string ma[30])
+{
+	int i;
+	i = 0;
+	students temp;
+	//string smj;
+	students *p = new students;
+	students *pp = new students;
+	p = stu;
+	while (ma[i][0] != '\0') 
+	{
+		while (p != NULL) 
+		{
+			//smj = p->get_major();
+			if (p->get_major() != ma[i]) 
+			{
+				pp = p->get_next();
+				while (pp != NULL) 
+				{
+					if (pp->get_major() == ma[i]) 
+					{
+						temp.scpy(pp);
+						pp->scpy(p);
+						p->scpy(&temp);
+					}//swap
+					pp = pp->get_next();
+				}
+			}
+			p = p->get_next();
+		}
+		i++;
+	}
+	p = stu;
+	p->input(p);
+}
+
+void MajorAdd(cgrade g[], string maj[30], int it) 
+{
+	while (maj[it][0] != '\0')
+	{
+		g[it].set_major(maj[it]);
+		it++;
+	}
+}
